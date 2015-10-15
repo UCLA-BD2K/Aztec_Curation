80d36383709d008b25ab8497b04163a06c885fba
Vincent Kyi <pinglab_dev1@Vincents-MacBook-Pro.local>
2015-10-15 10:52:53 -0700

    clean up

--- app.js
+++ app.js
@@ -4,24 +4,21 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mysql      = require('mysql');
-var myConnection = require('express-myconnection');
+var bookshelf = require('./config/bookshelf.js');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-var connection = require('./connect/mysql.js');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-app.use(myConnection(mysql, connection, 'pool'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,6 +34,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
 
 // error handlers
 
