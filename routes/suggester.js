aa50a207d482d63266202801d6b522eab8ba788f
Brian Bleakley <brianbleakley@gmail.com>
2015-10-16 16:13:33 -0700

    suggester now checks pubmed api for suggestions

--- routes/suggester.js
+++ routes/suggester.js
@@ -10,13 +10,32 @@ module.exports = {
 			callback(null);//name will never change in current implementation
 			break;
 		case "PRIMARY_PUB_LINK":
-			callback("pubmed.org/recommended");
+			var request = require('request');
+			var xmlParser = require('xml2json');
+
+			var options = {
+			  url: 'url'
+			};
+			options.url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=" + JSON.parse(toolMetadataJson).NAME + "&field=title";
+			//options.url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=RchyOptimyx&field=title";
+			request(options, function (error, response, body) {
+			  if (!error && response.statusCode == 200) {
+				var responseAsJson = JSON.parse(xmlParser.toJson(body));
+				//console.log("pubmed json:");
+				//console.log(responseAsJson);
+				var bestGuess = 0; //just pick one
+				var articleNumber = responseAsJson.eSearchResult.IdList.Id[bestGuess];
+				console.log("best article guess:");
+				console.log(articleNumber);
+				callback("http://www.ncbi.nlm.nih.gov/pubmed/" + articleNumber);
+				}
+			});
 			break;
 		case "SOURCE_LINK":
 			var request = require('request');
 
 			var options = {
-			  url: 'https://api.github.com/search/repositories?q=tetris',
+			  url: 'url',
 			  headers: {
 			    'User-Agent': 'bleakley'
 			  }
@@ -25,9 +44,12 @@ module.exports = {
 			request(options, function (error, response, body) {
 			  if (!error && response.statusCode == 200) {
 				var responseAsJson = JSON.parse(body);
-				var bestURLGuess = responseAsJson.items[0].url;//just pick the first one
-				console.log(bestURLGuess);
-				callback(bestURLGuess);
+				var bestGuess = 1; //just pick one
+				var bestGuessURL = responseAsJson.items[bestGuess].html_url;
+				var bestGuessDesc = responseAsJson.items[bestGuess].description;
+				console.log(bestGuessURL);
+				//callback("<a href=\"" + bestGuessURL + "\" target=\"_blank\">" + bestGuessURL + "</a> " + bestGuessDesc);
+				callback(bestGuessURL);
 			  }
 			});
 			break;
@@ -39,4 +61,11 @@ module.exports = {
   }
 };
 
+
+
+
+
+
+
+//http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=RchyOptimyx&field=title
 
