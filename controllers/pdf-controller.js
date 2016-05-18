cca91cc108953f63d4134effd7c5a8466aff1345
pitai <shashank2khanna@g.ucla.edu>
2016-05-18 15:19:32 -0700

    deleting completed

--- controllers/pdf-controller.js
+++ controllers/pdf-controller.js
@@ -3,12 +3,16 @@ var stripAnsi = require('strip-ansi');
 function Uploader(){
 	var self = this; 
 	self.upload = function(req, res){ self._upload(self, req, res); };
+  self.delete_file = function(req, res){ self._delete_file(self, req, res); };
 }
 
+
+
 Uploader.prototype._upload = function(self, req, res){
-
-  // setTimeout(function(){},waitTime);
-
+  // test
+  console.log("User is");
+  console.log(req.user);
+// pass the req.user object to the shell script in order to make specific folders and delete specific folders
   if(req.file){
   	var exec = require('child_process').exec;
   	var puts = ""; 
@@ -19,16 +23,10 @@ Uploader.prototype._upload = function(self, req, res){
     // console.log(error);
   }
     var a = JSON.stringify(stripAnsi(stdout), null, 3);
-      console.log(a);
     res.json(JSON.parse(a));
 
-});
-}	
-
-  
-
-
-
+    });
+  }	
 };
 
 Uploader.prototype._delete_file = function(self, req, res){
@@ -36,12 +34,10 @@ Uploader.prototype._delete_file = function(self, req, res){
   const execFile = require('child_process').execFile;
     const child = execFile('bash', ['/Users/davidmeng/Desktop/Aztec_Curation/slots-extraction/scripts/delete_file.sh'], (error, stdout, stderr) => {
   if (error) {
-    // console.log(error);
   }
       console.log(stdout);
-});  
+  });  
 
 }
-
 
 module.exports = new Uploader();
--- slots-extraction/scripts/delete_file.sh
+++ slots-extraction/scripts/delete_file.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # this script deletes the pdf in the uploads folder 
-echo $PWD
-echo 'I am deleting stuff now'
 rm -fr $PWD/slots-extraction/data/papers/* 
+rm -fr $PWD/slots-extraction/data/slotExtracts/* 
+rm -fr $PWD/slots-extraction/data/textExtracts/*
+rm -fr $PWD/slots-extraction/data/XMLExtracts/* 
+
 # do the same for text extracts, XMl extracts and slotExtracts
